dfe15287d5f3e5630b5fa8d4dd5a0c0b9b0e594a
lkeeeeeee <lke829@naver.com>
2023-12-21 13:21:35 +0900

    초기셋팅변경2

--- src/main/java/ictedu/control/Controller.java
+++ src/main/java/ictedu/control/Controller.java
@@ -16,7 +16,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import icteduaction.Action;
+import ictedu.action.Action;
 
 /**
  * Servlet implementation class Controller
