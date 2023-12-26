cd61112018fd6b573ced8609fba35b7382d0c674
sannabie <sksms0207@naver.com>
2023-12-26 09:49:32 +0900

    학적부 page, 학적부 list 추가

--- src/main/java/mybatis/dao/SchoolDAO.java
+++ src/main/java/mybatis/dao/SchoolDAO.java
@@ -1,5 +1,26 @@
 package mybatis.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import mybatis.service.FactoryService;
+import mybatis.vo.TraineeVO;
+
 public class SchoolDAO {
+	
+	public static TraineeVO[] getList() {
+		TraineeVO[] ar = null;
+		
+		SqlSession ss = FactoryService.getFactory().openSession();
+		List<TraineeVO> list = ss.selectList("trainee.all");
+		
+		if (list!= null && !list.isEmpty()) {
+			ar = new TraineeVO[list.size()];
+			list.toArray(ar);
+		}
+		ss.close();
+		return ar;
+	}
 
 }
