a8a07cd7060995924939a93d95195ad15e82dabf
hyeonaxax <kid05251@naver.com>
2023-12-27 09:44:31 +0900

    9 44

---
+++ src/main/java/mybatis/vo/TrainuploadVO.java
@@ -0,0 +1,66 @@
+package mybatis.vo;
+
+public class TrainuploadVO {
+	private String tn_idx, subject, content, file_name, ori_name, tn_name, status;
+
+	public String getTn_idx() {
+		return tn_idx;
+	}
+
+	public void setTn_idx(String tn_idx) {
+		this.tn_idx = tn_idx;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getFile_name() {
+		return file_name;
+	}
+
+	public void setFile_name(String file_name) {
+		this.file_name = file_name;
+	}
+
+	public String getOri_name() {
+		return ori_name;
+	}
+
+	public void setOri_name(String ori_name) {
+		this.ori_name = ori_name;
+	}
+
+	public String getTn_name() {
+		return tn_name;
+	}
+
+	public void setTn_name(String tn_name) {
+		this.tn_name = tn_name;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
+	
+	
+
+}
