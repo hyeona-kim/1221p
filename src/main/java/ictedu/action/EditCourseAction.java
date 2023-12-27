9cec82f75036753b5d3d1be42c20b8768b386d26
jeongjjy <wodud4611@naver.com>
2023-12-27 12:53:19 +0900

    커밋

---
+++ src/main/java/ictedu/action/EditCourseAction.java
@@ -0,0 +1,47 @@
+package ictedu.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import mybatis.dao.CourseDAO;
+import mybatis.vo.CourseVO;
+
+public class EditCourseAction implements Action {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) {
+		  String viewPath = null;
+		  
+		try {
+            // c_idx 파라미터 받아오기
+            String c_idx = request.getParameter("c_idx");
+
+            // c_idx를 기반으로 CourseVO 객체 가져오기
+            CourseVO vo = CourseDAO.getCourse(c_idx);
+
+            request.setAttribute("vo", vo);
+
+            // 폼으로부터 다른 파라미터들 가져오기
+            String c_name = request.getParameter("c_name");
+            String start_date = request.getParameter("start_date");
+            String end_date = request.getParameter("end_date");
+            String course_fee = request.getParameter("course_fee");
+            String c_peo_num = request.getParameter("c_peo_num");
+            String c_round_num = request.getParameter("c_round_num");
+
+            // CourseDAO의 edit 메소드 호출
+            CourseDAO.edit(c_name, start_date, end_date, course_fee, c_peo_num, c_round_num);
+
+            // 수정된 게시물을 보여주기 위한 경로 설정
+            viewPath = "Controller?type=course&listSelect=1";
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            // 예외 처리 코드 추가
+        }
+
+        return viewPath;
+    
+	}
+
+}
