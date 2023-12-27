8c0e0b5e05132e88b307f5d7c9683a1ca6b6f9d1
lkeeeeeee <lke829@naver.com>
2023-12-27 12:17:20 +0900

    1217

--- src/main/java/ictedu/action/SearchCourseAction.java
+++ src/main/java/ictedu/action/SearchCourseAction.java
@@ -20,24 +20,20 @@ public class SearchCourseAction implements Action{
 		// 검색을 해서 가져와서 page와, ar 을 가져와야 한다.
 		//만약에 검색창에 공백이 입력됐을때에는 ar에 null을 저장하고 움직 여야한다. 그럼 courseAction에서 ar을 새롭게 받아서 전체를 가져오는 ar로 지정해서 받기 때문이다.
 		// page는 page select에 입려되는 value값을 가져오고 
-		// selectYear에서의 연도는 개강일의 연도를 뜻한다 .
+		// selectYear에서의 연도는 개강일의 연도를 뜻한다.
+		request.removeAttribute("ar");
+		request.removeAttribute("page");
 		
 		System.out.println("검색");
 		String numPerPage = request.getParameter("num");
 		String year = request.getParameter("year");
 		String select = request.getParameter("select");
 		String value = request.getParameter("value");
-		
+		String listSelect = request.getParameter("listSelect");
+		String cPage = request.getParameter("cPage");
+		 
 		Paging page = null;
-
-		if(numPerPage!=null && numPerPage.length()>0 )
-			page = new Paging(Integer.parseInt(numPerPage),5);
-		else 
-			page = new Paging(); 
-		
-		page.setNowPage(2);
-		page.setTotalRecord(1);
-		
+		System.out.println(numPerPage);
 		
 		if(value == null || value.length()<1) {
 			value = null;
@@ -50,22 +46,9 @@ public class SearchCourseAction implements Action{
 		if(year == null || year.length()==0) {
 			year = null;
 		}
-		if(select != null)
-			System.out.println("선택한검색타입"+select);
-		else {
-			System.out.println("select null");
-		}
-		if(year != null)
-			System.out.println(year);
-		else {
-			System.out.println("year null");
-		}
-		if(value != null)
-			System.out.println("변환되기전 value값"+value);
-		else {
-			System.out.println("value null");
-		}
+		
 		LmsBean bean = new LmsBean();
+		
 		if(value != null && select.equals("1")) {
 			StaffVO vo = bean.searchStaff2(value);
 			value = vo.getSf_idx();
@@ -78,21 +61,39 @@ public class SearchCourseAction implements Action{
 		}
 		
 		System.out.println("변환된 value값"+value);
-		System.out.println(String.valueOf(page.getBegin())+String.valueOf(page.getEnd()));
+	
+
+		if(numPerPage!=null && numPerPage.length()>0 )
+			page = new Paging(Integer.parseInt(numPerPage),5);
+		else 
+			page = new Paging(); 
+		
+		if(cPage == null)
+	        page.setNowPage(1);
+	    else {
+	       int nowPage = Integer.parseInt(cPage);
+	       page.setNowPage(nowPage);
+	    }
+		
 		HashMap<String, String> map = new HashMap<String, String>();
 		map.put("year", year);
 		map.put("select", select);
 		map.put("value", value);
-		map.put("begin", String.valueOf(page.getBegin()));
-		map.put("end", String.valueOf(page.getEnd()));
 		
+		page.setTotalRecord(1);
+		System.out.println(page.getBegin());
+		map.put("begin", "0");
+		map.put("end", "5");
+		
+		
+		System.out.println(select);
 		CourseVO[] ar = CourseDAO.searchCourse(map);
 	
-		//System.out.println(ar.length);
+		System.out.println(ar);
 		request.setAttribute("ar", ar);
 		request.setAttribute("page", page);
 		
-		return "Controller?type=course";
+		return "Controller?type=course&listSelect="+listSelect;
 	}
 	
 }
